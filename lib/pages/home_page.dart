e0e01231fe0bf7cd336aa1caeebdf38933fca101
Hakimbek <hakimbekmrx@gmail.com>
2023-06-22 19:26:42 +0500

    maybe the end

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'package:camera/camera.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
